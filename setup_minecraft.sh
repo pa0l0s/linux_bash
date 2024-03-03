93c4cc0bce37bb730a253debfb80dcff39e03335
pa0l0s <pawelgr@gmail.com>
2024-03-03 15:04:48 +0100

    setup minecraft update

--- setup_minecraft.sh
+++ setup_minecraft.sh
@@ -10,9 +10,23 @@ wget https://api.papermc.io/v2/projects/paper/versions/1.20.4/builds/436/downloa
 
 echo "eula=true" >> eula.txt
 
-echo "[Unit]\nDescription=Minecraft server\nAfter=network.target\nStartLimitIntervalSec=500\nStartLimitBurst=5\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=5s\nWorkingDirectory=/opt/minecraft\nExecStart=java -Xms2G -Xmx4G -jar /opt/minecraft/server.jar --nogui\n[Install]\nWantedBy=multi-user.target\n" >> /etc/systemd/system/minecraft.service
+echo "[Unit]
+Description=Minecraft server
+After=network.target
+StartLimitIntervalSec=500
+StartLimitBurst=5
+[Service]
+Type=simple
+Restart=on-failure
+RestartSec=5s
+WorkingDirectory=/opt/minecraft
+ExecStart=java -Xms2G -Xmx4G -jar /opt/minecraft/paper-1.20.4-436.jar --nogui
+[Install]
+WantedBy=multi-user.target" >> /etc/systemd/system/minecraft.service
 
 systemctl daemon-reload
 
 systemctl enable minecraft
+
+systemctl start minecraft
 
